cfb3a2336b16a01f1defc5cc3ebc311476df3f4c
leonorfmartins <leonorfmartins@gmail.com>
2024-04-10 11:15:54 +0200

    Add update methods for the dual writer

--- pkg/apiserver/rest/dualwriter_mode4.go
+++ pkg/apiserver/rest/dualwriter_mode4.go
@@ -33,3 +33,22 @@ func (d *DualWriterMode4) Get(ctx context.Context, name string, options *metav1.
 func (d *DualWriterMode4) Delete(ctx context.Context, name string, deleteValidation rest.ValidateObjectFunc, options *metav1.DeleteOptions) (runtime.Object, bool, error) {
 	return d.Storage.Delete(ctx, name, deleteValidation, options)
 }
+
+// Update overrides the generic behavior of the Storage and writes only to US.
+func (d *DualWriterMode4) Update(ctx context.Context, name string, objInfo rest.UpdatedObjectInfo, createValidation rest.ValidateObjectFunc, updateValidation rest.ValidateObjectUpdateFunc, forceAllowCreate bool, options *metav1.UpdateOptions) (runtime.Object, bool, error) {
+	old, err := d.Storage.Get(ctx, name, &metav1.GetOptions{})
+	if err != nil {
+		return nil, false, err
+	}
+
+	updated, err := objInfo.UpdatedObject(ctx, old)
+	if err != nil {
+		return nil, false, err
+	}
+	objInfo = &updateWrapper{
+		upstream: objInfo,
+		updated:  updated,
+	}
+
+	return d.Storage.Update(ctx, name, objInfo, createValidation, updateValidation, forceAllowCreate, options)
+}
