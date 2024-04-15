d0605b58e2f7a88b56f13600065f3bc116ffda68
joshhunt <josh@trtr.co>
2024-04-15 20:11:01 +0100

    I18n: Support for Enterprise translations

--- public/locales/i18next-parser-enterprise.config.js
+++ public/locales/i18next-parser-enterprise.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  defaultNamespace: 'grafana',
+  defaultNamespace: 'grafana-enterprise',
 
   // Adds changes only to en-US when extracting keys, every other language is provided by Crowdin
   locales: ['en-US'],
 
-  input: ['../../public/**/*.{tsx,ts}', '!../../public/app/extensions/**/*', '../../packages/grafana-ui/**/*.{tsx,ts}'],
+  input: ['../../public/app/extensions/**/*.{tsx,ts}'],
 
-  output: './public/locales/$LOCALE/$NAMESPACE.json',
+  output: './public/app/extensions/locales/$LOCALE/$NAMESPACE.json',
 
   sort: true,
 
